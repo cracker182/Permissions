1599fd97d9c423403901d287ac8f4a3ba9f7101c
24aitor <1aitorribaarchilla@gmail.com>
2017-02-22 17:47:47 +0100

    Fixes

--- src/Controllers/PermissionController.php
+++ src/Controllers/PermissionController.php
@@ -44,33 +44,29 @@ class PermissionController extends Controller
             'description' => 'required|max:500',
         ]);
 
-        Permission::create([
-            'name' => $request->input('name'),
-            'description' => $request->input('description'),
-            'slug' => $request->input('slug'),
-        ]);
+        Permission::create($request->all());
         return redirect()->route('laralum::permissions.index')->with('success','Permission added!');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param \Laralum\Permission\Models\Permission $permission
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Permission $permission)
     {
-        return view('laralum_permissions::edit', ['permission' => Permission::findOrFail($id)]);
+        return view('laralum_permissions::edit', ['permission' => $permission]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param \Laralum\Permission\Models\Permission $permission
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Permission $permission)
     {
 
         if (str_replace(' ', '', $request->slug) != $request->slug) {
@@ -78,46 +74,37 @@ class PermissionController extends Controller
         }
         $this->validate($request, [
             'name' => 'required|max:255',
-            'slug' => 'required|max:255|unique:laralum_permissions,slug,'.$id,
+            'slug' => 'required|max:255|unique:laralum_permissions,slug,'.$permission->id,
             'description' => 'required|max:500',
         ]);
 
-        $permission = Permission::findOrFail($id);
-
-        $permission->update([
-            'name' => $request->input('name'),
-            'description' => $request->input('description'),
-            'slug' => $request->input('slug'),
-        ]);
+        $permission->update($request->all());
         return redirect()->route('laralum::permissions.index')->with('success','Permission edited!');
     }
 
     /**
      * Displays a view to confirm delete.
      *
-     * @param  int  $id
+     * @param \Laralum\Permission\Models\Permission $permission
      * @return \Illuminate\Http\Response
      */
-    public function confirmDelete($id)
+    public function confirmDelete(Permission $permission)
     {
-        $permission = Permission::findOrFail($id);
-
         return view('laralum::pages.confirmation', [
             'method' => 'DELETE',
-            'action' => route('laralum::permissions.destroy', ['permission' => $permission]),
+            'action' => route('laralum::permissions.destroy', ['permission' => $permission->id]),
         ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param \Laralum\Permission\Models\Permission $permission
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id, Request $request)
+    public function destroy(Request $request, Permission $permission)
     {
-        $permission = Permission::findOrFail($id);
-
         $permission->delete();
 
         return redirect()->route('laralum::permissions.index')->with('success','Permission deleted!');
