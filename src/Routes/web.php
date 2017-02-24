8cc47639504ff64ed786a99ef7f8fd09974d201d
Erik Campobadal <soc@erik.cat>
2017-02-24 20:21:58 +0100

    test

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -7,11 +7,8 @@ Route::group([
         ],
         'prefix' => config('laralum.settings.base_url'),
         'namespace' => 'Laralum\Permissions\Controllers',
-
         'as' => 'laralum::'
     ], function () {
-        // First the suplementor, then the resource
-        // https://laravel.com/docs/5.4/controllers#resource-controllers
         Route::get('permissions/{permission}/delete', 'PermissionController@confirmDelete')->name('permissions.destroy.confirm');
         Route::resource('permissions', 'PermissionController', ['except' => ['show']]);
 });
